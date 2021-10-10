5aac3163afcf7c19ed0a30dbdad22920fc560c6f
@tronghaitoank25 <tronghaitoank25@gmail.com>
2021-10-10 13:50:32 +0700

    update 4 player

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def init_grid(rows, cols, color):
 
     for i in range(rows):
         grid.append([])
-        for _ in range(cols):
+        for j in range(cols):
             grid[i].append(color)
 
     return grid
@@ -18,7 +18,7 @@ def init_grid(rows, cols, color):
 def draw_grid(win, grid):
     for i, row in enumerate(grid):
         for j, pixel in enumerate(row):
-            pygame.draw.rect(win, pixel, (j * PIXEL_SIZE, i *
+            pygame.draw.rect(win, pixel, ((j+6) * PIXEL_SIZE, i *
                                           PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
 
     if DRAW_GRID_LINES:
@@ -26,20 +26,18 @@ def draw_grid(win, grid):
             pygame.draw.line(win, BLACK, (TOOLBAR_WIDTH, i * PIXEL_SIZE),
                              (WIDTH - TOOLBAR_WIDTH, i * PIXEL_SIZE))
 
-        for i in range(6, COLS + 1):
+        for i in range(6, COLS + 7):
             pygame.draw.line(win, BLACK, (i * PIXEL_SIZE, 0),
                              (i * PIXEL_SIZE, HEIGHT - TOOLBAR_HEIGHT))
 
+    pygame.display.update()
 
-def draw(win, grid, buttons):
+def draw_buttons(win, buttons):
     win.fill(BG_COLOR)
-    draw_grid(win, grid)
-
     for button in buttons:
         button.draw(win)
 
     pygame.display.update()
-
 
 def get_row_col_from_pos(pos):
     x, y = pos
@@ -85,17 +83,17 @@ buttons = [
 ]
 
 # Draw by keyboard
-pos_x = 23;
+pos_x = 18;
 pos_y = 9;
 grid[pos_y][pos_x] = drawing_colors[0]
 
 # Setup player P1
-P1_pos_x = 6;
+P1_pos_x = 0;
 P1_pos_y = 0;
 grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
 
 # Setup player P2
-P2_pos_x = 6;
+P2_pos_x = 0;
 P2_pos_y = ROWS - 1;
 grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
 
@@ -116,10 +114,11 @@ players = [
     Player(P3_pos_x, P3_pos_y, 1, drawing_colors[3]),
     Player(P4_pos_x, P4_pos_y, 1, drawing_colors[4]),
 ]
+draw_buttons(WIN, buttons)
 
 while run:
     clock.tick(FPS)
-
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -135,7 +134,7 @@ while run:
         elif keys[pygame.K_RIGHT] and pos_x + 1 < COLS:
             pos_x +=  1;
             grid[pos_y][pos_x] = drawing_color
-        elif keys[pygame.K_LEFT] and pos_x > 6:
+        elif keys[pygame.K_LEFT] and pos_x > 0:
             pos_x -=  1;
             grid[pos_y][pos_x] = drawing_color
 
@@ -156,6 +155,19 @@ while run:
                         grid = init_grid(ROWS, COLS, BG_COLOR)
                         drawing_color = drawing_colors[0]
 
-    draw(WIN, grid, buttons)
+                        if button.text == "D1" and P1_pos_y  + 1 < ROWS:
+                            P1_pos_y +=  1;
+                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                        elif button.text == "U1" and P1_pos_y > 0:
+                            P1_pos_y -=  1;
+                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                        elif button.text == "R1" and P1_pos_x + 1 < COLS:
+                            P1_pos_x +=  1;
+                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                        elif button.text == "L1" and P1_pos_x > 6:
+                            P1_pos_x -=  1;
+                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+
+    draw_grid(WIN, grid)
 
 pygame.quit()
