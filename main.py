6c7dbbe11376ddbadf0b768fea0fb3a3d1c9ce37
@tronghaitoank25 <tronghaitoank25@gmail.com>
2021-10-10 16:18:37 +0700

    4 player oke

--- main.py
+++ main.py
@@ -41,11 +41,14 @@ def draw_buttons(win, buttons):
 
 def get_row_col_from_pos(pos):
     x, y = pos
+
+    if x < 180 or x > 1260:
+        raise IndexError
+    if y < 0 or y > 600:
+        raise IndexError
+
     row = y // PIXEL_SIZE
     col = x // PIXEL_SIZE
-
-    if row >= ROWS:
-        raise IndexError
 
     return row, col
 
@@ -140,6 +143,7 @@ while run:
 
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
+            
 
             try:
                 row, col = get_row_col_from_pos(pos)
@@ -154,19 +158,95 @@ while run:
                     if button.text == "Clear":
                         grid = init_grid(ROWS, COLS, BG_COLOR)
                         drawing_color = drawing_colors[0]
+                        # Draw by keyboard
+                        pos_x = 18;
+                        pos_y = 9;
+                        grid[pos_y][pos_x] = drawing_colors[0]
 
-                        if button.text == "D1" and P1_pos_y  + 1 < ROWS:
-                            P1_pos_y +=  1;
-                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
-                        elif button.text == "U1" and P1_pos_y > 0:
-                            P1_pos_y -=  1;
-                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
-                        elif button.text == "R1" and P1_pos_x + 1 < COLS:
-                            P1_pos_x +=  1;
-                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
-                        elif button.text == "L1" and P1_pos_x > 6:
-                            P1_pos_x -=  1;
-                            grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                        # Setup player P1
+                        P1_pos_x = 0;
+                        P1_pos_y = 0;
+                        grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+
+                        # Setup player P2
+                        P2_pos_x = 0;
+                        P2_pos_y = ROWS - 1;
+                        grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
+
+                        # Setup player P3
+                        P3_pos_x = COLS - 1;
+                        P3_pos_y = 0;
+                        grid[P3_pos_y][P3_pos_x] = drawing_colors[3]
+
+                        # Setup player P4
+                        P4_pos_x = COLS - 1;
+                        P4_pos_y = ROWS - 1;
+                        grid[P4_pos_y][P4_pos_x] = drawing_colors[4]
+
+                        #Init 4 players
+                        players = [
+                            Player(P1_pos_x, P1_pos_y, 1, drawing_colors[1]),
+                            Player(P2_pos_x, P2_pos_y, 1, drawing_colors[2]),
+                            Player(P3_pos_x, P3_pos_y, 1, drawing_colors[3]),
+                            Player(P4_pos_x, P4_pos_y, 1, drawing_colors[4]),
+                        ]
+                        draw_buttons(WIN, buttons)
+
+                    #P1
+                    if button.text == "D1" and P1_pos_y + 1 < ROWS:
+                        P1_pos_y +=  1;
+                        grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                    elif button.text == "U1" and P1_pos_y > 0:
+                        P1_pos_y -=  1;
+                        grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                    elif button.text == "R1" and P1_pos_x + 1 < COLS:
+                        P1_pos_x +=  1;
+                        grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+                    elif button.text == "L1" and P1_pos_x > 0:
+                        P1_pos_x -=  1;
+                        grid[P1_pos_y][P1_pos_x] = drawing_colors[1]
+
+                    #P2
+                    if button.text == "D2" and P2_pos_y + 1 < ROWS:
+                        P2_pos_y +=  1;
+                        grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
+                    elif button.text == "U2" and P2_pos_y > 0:
+                        P2_pos_y -=  1;
+                        grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
+                    elif button.text == "R2" and P2_pos_x + 1 < COLS:
+                        P2_pos_x +=  1;
+                        grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
+                    elif button.text == "L2" and P2_pos_x > 0:
+                        P2_pos_x -=  1;
+                        grid[P2_pos_y][P2_pos_x] = drawing_colors[2]
+
+                    #P3
+                    if button.text == "D3" and P3_pos_y + 1 < ROWS:
+                        P3_pos_y +=  1;
+                        grid[P3_pos_y][P3_pos_x] = drawing_colors[3]
+                    elif button.text == "U3" and P3_pos_y > 0:
+                        P3_pos_y -=  1;
+                        grid[P3_pos_y][P3_pos_x] = drawing_colors[3]
+                    elif button.text == "R3" and P3_pos_x + 1 < COLS:
+                        P3_pos_x +=  1;
+                        grid[P3_pos_y][P3_pos_x] = drawing_colors[3]
+                    elif button.text == "L3" and P3_pos_x > 0:
+                        P3_pos_x -=  1;
+                        grid[P3_pos_y][P3_pos_x] = drawing_colors[3]
+
+                    #P4
+                    if button.text == "D4" and P4_pos_y + 1 < ROWS:
+                        P4_pos_y +=  1;
+                        grid[P4_pos_y][P4_pos_x] = drawing_colors[4]
+                    elif button.text == "U4" and P4_pos_y > 0:
+                        P4_pos_y -=  1;
+                        grid[P4_pos_y][P4_pos_x] = drawing_colors[4]
+                    elif button.text == "R4" and P4_pos_x + 1 < COLS:
+                        P4_pos_x +=  1;
+                        grid[P4_pos_y][P4_pos_x] = drawing_colors[4]
+                    elif button.text == "L4" and P4_pos_x > 0:
+                        P4_pos_x -=  1;
+                        grid[P4_pos_y][P4_pos_x] = drawing_colors[4]
 
     draw_grid(WIN, grid)
 
